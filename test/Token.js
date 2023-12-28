e70dbdf078ff247c9f750c20e5f05bc6ddb79b1d
Justin Kelly <justinkelly@justinkelly.net>
2023-12-28 14:31:47 +0100

    7 token transfer delegation

--- test/Token.js
+++ test/Token.js
@@ -11,7 +11,6 @@ const tokenData = {
 }
 
 describe('Token', () => {
-
     let token, accounts, deployer, receiver, exchange
 
     beforeEach(async () => {
@@ -25,6 +24,7 @@ describe('Token', () => {
     })
 
     describe('Deployment', () => {
+
         it('has correct name', async () => {
             expect(await token.name()).to.equal(tokenData.name)
         })
@@ -117,6 +117,52 @@ describe('Token', () => {
                 await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
             })
         })
+    })
+
+    describe('Delegated token transfers', () => {
+        let amount, transaction, result
+
+        beforeEach(async () => {
+            amount = tokens(100)
+            transaction = await token.connect(deployer).approve(exchange.address, amount)
+            result = await transaction.wait()
+        })
+
+        describe('Success', () => {
+
+            beforeEach(async () => {
+                transaction = await token.connect(exchange).transferFrom(deployer.address, receiver.address, amount)
+                result = await transaction.wait()
+            })
+
+            it('transfers token balances', async () => {
+                expect(await token.balanceOf(deployer.address)).to.equal(ethers.utils.parseUnits('999900', 'ether'))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+            })
+
+            it('resets the allowance', async () => {
+                expect(await token.allowance(deployer.address, exchange.address)).to.be.equal(0)
+            })
+
+            it('Emits a Transfer event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Transfer')
+
+                const args = event.args
+                expect(args.from).to.equal(deployer.address)
+                expect(args.to).to.equal(receiver.address)
+                expect(args.value).to.equal(amount)
+            })
+        })
+
+        describe('Failure', () => {
+            const invalidAmount = tokens(100000000)
+
+            it('transfers invalidAmount of tokens', async () => {
+                await expect(token.connect(exchange).transferFrom(deployer.address, receiver.address, invalidAmount)).to.be.reverted
+            })
+        })
+
     })
 })
 
